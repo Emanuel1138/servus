ba15b2904ea599188dc2e71ef006077eb54ec13a
Emanuel1138 <emanuelrcsousa@gmail.com>
2025-11-20 21:52:47 -0300

    feat(layout): criar pasta layouts e view app.blade.php com estilização da welcome

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css') 
+</head>
+<body class="bg-red-50">
+    <header>
+        <nav class="navbar flex justify-between pt-[20px] pr-[160px] pl-[160px]">
+            <div class="logo">
+                <a href="{{ route('welcome') }}"><img class="w-[170px]" src="{{ asset('images/logo.svg') }}" alt="Logo"></a>
+            </div>
+            <ul class="nav-links flex gap-[30px] items-center">
+                <li><a href="" class="hover:underline">Início</a></li>
+                <li><a href="" class="hover:underline">funcionalidades</a></li>
+                <li><a href="" class="hover:underline">Sobre</a></li>
+            </ul>
+            <div class="login-signup flex items-center gap-4">
+                <button class="border border-transparent text-black px-4 py-2 rounded-3xl hover:border-black transition">
+                    Login
+                </button>
+                <button class="border-transparent border bg-black text-red-50 rounded-3xl font-semibold px-6 py-2 hover:bg-red-50 hover:border hover:border-black  hover:text-black transition">
+                    Começar Agora
+                </button>
+            </div>
+        </nav> 
+    </header>
+     
+    <div class="hero">
+
+    </div>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <p>&copy; 2025 Servus</p>
+    </footer>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,9 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
-  </head>
-  <body>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </body>
-</html>
+@extends('layouts.app')
+
+@section('tile', 'Servus')
+
+@section('content')
+    <section>
+        <h2 class="text-red-600">Bem-vindo ao Servus</h2>
+    </section>
+@endsection
