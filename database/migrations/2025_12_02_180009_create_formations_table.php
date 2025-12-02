8e217114d5ce24ee43acb1e6c48d0176c5aa3391
Emanuel1138 <emanuelrcsousa@gmail.com>
2025-12-02 15:20:20 -0300

    feat(formations): criação do model, migration e controller de formations

---
+++ database/migrations/2025_12_02_180009_create_formations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('formations', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignId('group_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->longText('body_html');
+            $table->json('body_delta')->nullable();
+            $table->boolean('is_public')->default(false);
+
+            $table->foreignId('last_edited_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+            $table->timestamps();
+            
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('formations');
+    }
+};
