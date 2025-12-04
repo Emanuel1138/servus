8d41ccaee3add93a746a920bd68352914f43bb35
unknown <Alice333-09>
2025-12-04 13:57:33 -0300

    feat(ui): ajustar design dos layouts de cadastro e login de usuário — corrige flexbox, remove estilos inline, melhora responsividade e ajusta espaçamentos e fontes

--- resources/views/layouts/userform.blade.php
+++ resources/views/layouts/userform.blade.php
@@ -7,33 +7,31 @@
     <title>@yield('title')</title>
     @vite('resources/css/app.css') 
 </head>
-<body class="flex flex-row w-full  bg-slate-950 h-screen" style="background-image: url('/images/background.png'); background-size: cover; background-position: center;">
+<body class="flex flex-row w-full h-screen bg-cover bg-center bg-slate-950" 
+      style="background-image: url('/images/background.png');">
 
-    <div class="w-[45%] h-[105vh] flex-col align-center bg-red-50 pb-10">
-        <header class="logo w-full">
-            <div class="p-[40px] pb-0">
-                <img class="w-[140px]" src="{{ asset('images/logo.svg') }}" alt="">
-            </div>
+    <div class="w-[40%] h-full flex flex-col bg-red-50 pb-10">
+        <header class="p-10 pb-0">
+            <img class="w-36" src="{{ asset('images/logo.svg') }}" alt="">
         </header>
 
-        <div class="form-name w-full flex justify-center">
-            <h1 class="font-robotoSerif font-medium text-xl">@yield('form-name')</h1>
-        </div>
+        <div class="flex flex-col justify-center flex-1">
+            <h1 class="font-robotoSerif font-medium text-xl text-center">@yield('form-name')</h1>
 
-        <div class="form pt-[30px] w-full flex justify-center">
-
-            <div class="flex-none w-[400px]">
-                @yield('content')
+            <div class="pt-8 flex justify-center">
+                <div class="w-[400px]">
+                    @yield('content')
+                </div>
             </div>
-            
         </div>
     </div>
 
-    <div class="w-[55%] flex justify-center items-center p-20 font-sourceSerif ">
-        <div class="bg-red-50 p-10 w-[400px] rounded-lg shadow-lg">
-            <p class="italic text-2xl mb-[30px]">“Os ministros devem desempenhar suas funções com sincera piedade e ordem, conforme as normas litúrgicas, evitando toda preferência pessoal.”</p>
+    <div class="w-[60%] flex justify-center items-center p-20 font-sourceSerif">
+        <div class="bg-red-50 p-10 w-[500px] h-[390px] rounded-lg shadow-lg">
+            <p class="italic text-3xl mb-8">
+                “Os ministros devem desempenhar suas funções com sincera piedade e ordem, conforme as normas litúrgicas, evitando toda preferência pessoal.”
+            </p>
             <p class="flex justify-end text-lg">IGMR, n. 94</p>
-            
         </div>
     </div>
 </body>
